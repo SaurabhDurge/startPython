bc5963eddc6ccdf0c9feacb7770f03745e8bd03a
smax <saurabhdurge@gmail.com>
2018-09-22 18:08:45 +0530

    created new project

---
+++ cls_obj_python/shark.py
@@ -0,0 +1,18 @@
+class Shark:
+	def swim(self):
+		print("The Shark is swimming.")
+	def be_awesome(self):
+		print("The Shark is awesome.")
+		
+def main():
+	sammy 	= Shark()
+	sammy.swim()
+	sammy.be_awesome()
+
+if __name__ == "__main__":
+	main()
+	
+else:
+	print("Rest part execution.")
+
+		
